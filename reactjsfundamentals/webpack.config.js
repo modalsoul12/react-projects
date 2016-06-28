19f3fde81b8ba5df6db68df9b6013d057bef50da
Victor Pham <victorpham1@gmail.com>
2016-06-28 15:38:28 -0500

    Misc fixes

--- reactjsfundamentals/webpack.config.js
+++ reactjsfundamentals/webpack.config.js
@@ -1,24 +1,20 @@
-//Plugin to automatically replace index.html for the production version of our app.
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/views/index.html',
+  template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
 module.exports = {
-    //Entry point for application
     entry: [
-        './app/views/index.js'
-    ],
+        './app/index.js'
+        ],
     module: {
-        //What webpack will affect, exclude
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
     },
     output: {
-        //Output file and directory
         filename: "index_bundle.js",
         path: __dirname + '/dist'
   },
