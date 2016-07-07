a26e7bb5e9205e035332367430e09ca0085e931c
Victor Pham <victorpham1@gmail.com>
2016-07-07 14:29:18 -0500

    Add container directory with new container

--- reactjsfundamentals/app/config/routes.js
+++ reactjsfundamentals/app/config/routes.js
@@ -8,11 +8,14 @@ var hashHistory = ReactRouter.hashHistory;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <IndexRoute component={Home} />      
+      <IndexRoute component={Home} />
+      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' component={PromptContainer} />
     </Route>
   </Router>
 );
